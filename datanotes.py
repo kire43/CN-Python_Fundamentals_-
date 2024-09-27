20e967e239ca62def644a8007f63073312f89c05
Erik G <erik.gacoscos98@hotmail.com>
2024-09-27 17:28:07 +0100

    Data notes

---
+++ datanotes.py
@@ -0,0 +1,45 @@
+print ("This is data notes")
+# print () is used tto show outputs in the terminal
+# print ("this is data notes")
+
+#print ("This is a string")
+#print ("1234")
+# Both are strings
+# Even though the second line has digits on, they have no mathematical value - they're just characters
+#print (1234)
+# This is an integer. It is the number 1234. It has mathematical Value.
+#print (12.34)
+# This is a floating point. It has mathematical Value.
+#print (None)
+# This is None - it is empty, or has no value.
+#print (True)
+#print (False)
+# These are booleans. Booleans can either be true or false.
+
+print("hello world".upper())
+
+print(len("hello world"))
+
+print("|   |   |")
+print("|   |   |")
+print("|   |   |")
+print("---------")
+print("|   |   |")
+print("|   |   |")
+print("|   |   |")
+print("---------")
+print("|   |   |")
+print("|   |   |")
+print("|   |   |")
+
+print("The quick brown fox".lower())
+
+print("the quick BROWN fox".capitalize())
+
+print("the quick brown fox".count())
+
+print("the quick brown fox".find())
+
+print("the quick brown fox".replace())
+
+print("the quick brown fox".strip())
